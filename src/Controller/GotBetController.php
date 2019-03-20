cd420001106f92b3051a3839e9d1d13dc7ffcb6b
Lucas <lucasroux72@gmail.com>
2019-03-20 11:59:09 +0100

    modif ctrleur/Perso

--- src/Controller/GotBetController.php
+++ src/Controller/GotBetController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Personnage;
 use App\Entity\Question;
+use App\Entity\Reponse;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\Security\Core\User\UserInterface;
+//use Symfony\Component\Routing\Annotation\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GotBetController extends AbstractController
 {
@@ -34,5 +39,30 @@ class GotBetController extends AbstractController
             'personnages' => $personnages,
             'questions' => $questions
         ]);
+    }
+
+    /**
+     * @Route("/gotbet/createReponse", name="createReponse", methods="POST")
+     */
+    public function createReponse(Request $request){
+        $repo = $this->getDoctrine()->getRepository(Personnage::class);
+        $personnages = $repo->findAll();
+        var_dump($request->request);
+        //var_dump($request->request->get("statut_1"));
+        foreach( $request->request as $saisie){
+          //  var_dump($saisie);
+        }
+
+        foreach($personnages as $p){
+            $pid = $p->id;
+            var_dump($pid);
+            $pstatut = $request->request->statut_{{$pid}};
+            var_dump($pstatut);
+        }
+
+        return $this->render('got_bet/questionnaire.html.twig', [
+        'controller_name' => 'GotBetController',
+        'personnages' => $personnages
+    ]);
     }
 }
