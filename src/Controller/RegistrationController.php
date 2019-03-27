05ee75e8cceb47181a402f9697d9493cacc37dc2
Lucas <lucasroux72@gmail.com>
2019-03-27 17:03:07 +0100

    Captcha bug

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -19,6 +19,16 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
+        $secret = "6LdRWJoUAAAAANSolQM458YHxksRIber8MZluDbi";
+        $response = $_POST['g-recaptcha-response'];
+        $remoteip = $_SERVER['REMOTE_ADDR'];
+        $api_url ="https://www.google.com/recaptcha/api/siteverify?secret="
+          .$secret
+          ."&response=".$response
+          ."&remoteip=".$remoteip;
+        $decode = json_decode(file_get_contents($api_url), true);
+        if($decode['success'] == true){
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -49,5 +59,6 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+        }
     }
 }
